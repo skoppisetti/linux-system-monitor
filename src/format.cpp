211843e1845217cdc8f7621ab19e3831dd8c6259
Suresh Koppisetti <suresh.koppisetti@gmail.com>
2020-05-22 21:42:07 +0000

    Implemented the Format::ElapsedTime() method

--- src/format.cpp
+++ src/format.cpp
@@ -1,4 +1,6 @@
 #include <string>
+#include <math.h>
+#include <algorithm>
 
 #include "format.h"
 
@@ -8,4 +10,19 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+// string Format::ElapsedTime(long seconds[[maybe_unused]]) { 
+string Format::ElapsedTime(long seconds) { 
+    double hr_frac = seconds/3600.0;
+    double min_frac = seconds/60.0;
+    long hours = floor(hr_frac);
+    long mins = floor(min_frac - (hours * 60));
+    long secs = (min_frac - (hours * 60) - mins) * 60;
+    string hh = std::to_string(hours);
+    int hh_l = hh.length();
+    hh.insert(hh.begin(), std::max(2, hh_l) - hh_l, '0');
+    string mm = std::to_string(mins);
+    mm.insert(mm.begin(), 2 - mm.length(), '0');
+    string ss = std::to_string(secs);
+    ss.insert(ss.begin(), 2 - ss.length(), '0');
+    return hh + ":" + mm + ":" + ss; 
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
+#include <iostream>
+
 #include "ncurses_display.h"
 #include "system.h"
+#include "format.h"
 
 int main() {
-  System system;
-  NCursesDisplay::Display(system);
+  // Testing - TBD
+  std::cout << "77665 Seconds: " << Format::ElapsedTime(77665) <<std::endl;
+
+  // End - Testing
+  
+  // System system;
+  // NCursesDisplay::Display(system);
 }
